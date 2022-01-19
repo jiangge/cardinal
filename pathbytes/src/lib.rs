be9ff1c822563b0bf2df773d3b5725674ed94f02
Liu Dingming <liudingming@bytedance.com>
2022-01-20 02:26:11 +0800

    Add pathbytes

---
+++ pathbytes/src/lib.rs
@@ -0,0 +1,35 @@
+use std::path::Path;
+
+#[cfg(target_family = "windows")]
+pub fn p2b(path: &Path) -> &[u8] {
+    // Path->OsStr->Slice->Wtf8->[u8]
+    unsafe { std::mem::transmute(path) }
+}
+#[cfg(target_family = "unix")]
+pub fn p2b(path: &Path) -> &[u8] {
+    use std::os::unix::ffi::OsStrExt;
+    path.as_os_str().as_bytes()
+}
+#[cfg(target_family = "wasm")]
+pub fn p2b(path: &Path) -> &[u8] {
+    use std::os::wasm::ffi::OsStrExt;
+    path.as_os_str().as_bytes()
+}
+
+#[cfg(target_family = "windows")]
+pub fn b2p(bytes: &[u8]) -> &Path {
+    // Path->OsStr->Slice->Wtf8->[u8]
+    unsafe { std::mem::transmute(bytes) }
+}
+#[cfg(target_family = "unix")]
+pub fn b2p(bytes: &[u8]) -> &Path {
+    use std::ffi::OsStr;
+    use std::os::unix::ffi::OsStrExt;
+    Path::new(OsStr::from_bytes(bytes))
+}
+#[cfg(target_family = "wasm")]
+pub fn b2p(bytes: &[u8]) -> &Path {
+    use std::ffi::OsStr;
+    use std::os::wasm::ffi::OsStrExt;
+    Path::new(OsStr::from_bytes(bytes))
+}
